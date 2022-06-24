dd42c3d93b83161d9a812fe34b689b1652069e39
gihanjayakuru <gihan22603@gmail.com>
2022-06-24 14:48:04 +0530

    course start. v1.0

---
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textHello = findViewById(R.id.text1);
+
+    }
+}
