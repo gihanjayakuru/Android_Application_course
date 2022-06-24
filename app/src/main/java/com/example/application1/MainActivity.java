3870a108d1a5f0805d00c0a9830734bf5ce5daca
gihanjayakuru <gihan22603@gmail.com>
2022-06-24 19:01:05 +0530

    v1.02
    Display the input text field text

--- app/src/main/java/com/example/application1/MainActivity.java
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBtnClick (View view){
         TextView textHello = findViewById(R.id.txtMessage);
-        textHello.setText("HELLO!!");
+        EditText edtTxtName = findViewById(R.id.editTxtName);
+
+        textHello.setText(edtTxtName.getText().toString());
     }
 }
